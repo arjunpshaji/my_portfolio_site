fb5c569825d524d58afc8ec929e7f532cc5478e2
arjunpspadiyoor10@gmail.com <arjunpshaji@techfriar.com>
2025-04-27 15:36:49 +0530

    added lottie

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:my_portfolio/modules/bio/bio_widget.dart';
 import 'package:my_portfolio/modules/contact/view/contacts_section.dart';
 import 'package:my_portfolio/modules/experience/view/experience_section.dart';
@@ -7,6 +8,7 @@ import 'package:my_portfolio/modules/hero/view/hero_section.dart';
 import 'package:my_portfolio/modules/projects/view/project_section.dart';
 import 'package:my_portfolio/modules/skills/view/skills_section.dart';
 import 'package:my_portfolio/theme/app_theme.dart';
+import 'package:my_portfolio/theme/widgets/animated_background.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -19,18 +21,23 @@ class HomePage extends StatelessWidget {
         physics: BouncingScrollPhysics(),
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            // crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              HeroSection(),
-              BioWidget(),
-              SizedBox(height: 60),
-              SkillsSection(),
-              SizedBox(height: 60),
-              ExperienceSection(),
-              ProjectsSection(),
-              ContactSection(),
-              FooterSection(),
+          child: Stack(
+            children: [
+              Lottie.asset('assets/lottie/shooting_star.json', repeat: true, width: double.infinity, fit: BoxFit.cover),
+              // Lottie.asset('assets/lottie/snow.json', repeat: true, width: double.infinity, fit: BoxFit.cover),
+              Column(
+                children: const [
+                  HeroSection(),
+                  BioWidget(),
+                  SizedBox(height: 60),
+                  SkillsSection(),
+                  SizedBox(height: 60),
+                  ExperienceSection(),
+                  ProjectsSection(),
+                  ContactSection(),
+                  FooterSection(),
+                ],
+              ),
             ],
           ),
         ),
